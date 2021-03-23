697e4a174e5ff2b6db21da0d63d9b818bcc15b0f
VEmreBarty <79910653+VEmreBarty@users.noreply.github.com>
2021-03-24 00:42:41 +0300

    emre

--- MainScreen.js
+++ MainScreen.js
@@ -4,10 +4,14 @@ import {View,Text,Button } from 'react-native';
 const MainScreen = props => {
 
     return (
-        <View style={{flex:1,alignItems:"center"}} >
+        <View style={{ flex: 1, alignItems: "center" }} >
+            <View>
+                <Text>veysel emre arik</Text>
+            </View>
             <Text>Anasayfa</Text>
             <Button title="diger sayfa" onPress={() => { }} />
         </View>
+        
     )
 
 
