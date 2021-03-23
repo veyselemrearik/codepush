95c1263ea1414dd9fb3c0be6d415c96b94f93871
VEmreBarty <79910653+VEmreBarty@users.noreply.github.com>
2021-03-23 22:57:07 +0300

    first-commit

---
+++ MainScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View,Text,Button } from 'react-native';
+
+const MainScreen = props => {
+
+    return (
+        <View style={{flex:1,alignItems:"center"}} >
+            <Text>Anasayfa</Text>
+            <Button title="diger sayfa" onPress={() => { }} />
+        </View>
+    )
+
+
+
+}
+
+
+export default MainScreen
