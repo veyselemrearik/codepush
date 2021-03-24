2286fcc04137a8ca03b6d05f78b162893bc7659b
VEmreBarty <79910653+VEmreBarty@users.noreply.github.com>
2021-03-25 01:20:47 +0300

    commit

--- App.tsx
+++ App.tsx
@@ -71,9 +71,9 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="emre arik">
+            Edit <Text>duzenlendi</Text>
+            deneme yazisi
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
